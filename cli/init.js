bd61da0934d42209e0265cfbe17f30882c8fd77d
Robert Hurst <rhurst@e-xact.com>
2015-09-08 17:22:19 -0700

    Add MongoDB template NomadFile.js via init

--- cli/init.js
+++ cli/init.js
@@ -4,18 +4,29 @@ var chalk = require('chalk');
 var nomad = require('../');
 
 
-module.exports = function() {
+module.exports = function(yargs) {
+  var argv = yargs.usage('Usage: $0 [type]')
+    .example('$0 mongo')
+    .example('$0')
+    .help('h')
+    .argv;
+
   console.log();
   console.log(chalk.white.bold('Initialize Nomad'));
   console.log();
 
-  nomad.init(function(err, result) {
+  nomad.init(argv._[1], function(err, result) {
     if (err) { throw err; }
     var nomadFilePath = path.relative(process.cwd(), result.path);
     if (result.isNew) {
-      console.log('Nomad file created at ' + chalk.cyan('./' + nomadFilePath));
+      console.log(
+        'Nomad file created at ' + chalk.cyan('./' + nomadFilePath) +
+        ' using the ' + chalk.cyan(result.templateName) + ' template'
+      );
     } else {
-      console.log('Nomad file already exists at ' + chalk.cyan('./' + nomadFilePath));
+      console.log(
+        'Nomad file already exists at ' + chalk.cyan('./' + nomadFilePath)
+      );
     }
   });
 };
--- template/NomadFile-mongodb.js
+++ template/NomadFile-mongodb.js
@@ -1,13 +1,18 @@
 var MongoClient = require('mongodb').MongoClient;
 
 
+// Remember to set these values to match your application's database
+var DATABASE_URL              = 'mongodb://localhost/my-app';
+var MIGRATION_COLLECTION_NAME = 'migrations';
+
+
 module.exports = function(nomad) {
 
   nomad.driver({
     connect: function(cb) {
       var _this = this;
 
-      MongoClient.connect('mongodb://localhost/{{database}}', function(err, db) {
+      MongoClient.connect(DATABASE_URL, function(err, db) {
         if (err) { return cb(err); }
         _this.db = db;
         cb(null, db);
@@ -19,11 +24,11 @@ module.exports = function(nomad) {
     },
 
     createMigration: function(migration, cb) {
-      this.db.collection('{{collection}}').insertOne(migration, cb);
+      this.db.collection(MIGRATION_COLLECTION_NAME).insertOne(migration, cb);
     },
 
     updateMigration: function(filename, migration, cb) {
-      this.db.collection('{{collection}}').updateOne({
+      this.db.collection(MIGRATION_COLLECTION_NAME).updateOne({
         filename: filename
       }, {
         $set: migration
@@ -31,7 +36,7 @@ module.exports = function(nomad) {
     },
 
     getMigrations: function(cb) {
-      this.db.collection('{{collection}}').find().toArray(cb);
+      this.db.collection(MIGRATION_COLLECTION_NAME).find().toArray(cb);
     }
   });
 };
