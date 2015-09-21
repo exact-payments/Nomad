a7ff53fc85d5c7f3adeba75238199d57d454d579
Robert Hurst <rhurst@e-xact.com>
2015-09-21 16:19:13 -0700

    NO-ISSUE - ensure help text is shown for invalid commands

--- cli/index.js
+++ cli/index.js
@@ -13,13 +13,22 @@ var log    = require('./log');
 
 yargs.usage('Usage: $0 <command>')
     .demand(1)
-    .command('init'  , 'Create a NomadFile.js in the current directory', init)
-    .command('create', 'Create a new migration in the migrations directory', create)
-    .command('up'    , 'Apply pending migrations', up)
-    .command('down'  , 'Rollback to a previous migration', down)
-    .command('log'   , 'Display a log of all migrations', log)
+    .command('init'  , 'Create a NomadFile.js in the current directory')
+    .command('create', 'Create a new migration in the migrations directory')
+    .command('up'    , 'Apply pending migrations')
+    .command('down'  , 'Rollback to a previous migration')
+    .command('log'   , 'Display a log of all migrations')
     .completion('completion', completion)
     .alias('h', 'help')
     .help('h')
     .epilogue(epilogueText.replace(/([^\n])\n([^\n])/g, '$1 $2'))
     .parse(process.argv);
+
+switch (yargs.argv._[0]) {
+  case 'init':   init(yargs.reset());   break;
+  case 'create': create(yargs.reset()); break;
+  case 'up':     up(yargs.reset());     break;
+  case 'down':   down(yargs.reset());   break;
+  case 'log':    log(yargs.reset());    break;
+  default:       yargs.showHelp();      break;
+}
